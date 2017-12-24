68e6734e8e2c72737b74cc93d06a481fb66f983b
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 11:36:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT Authors.name, Subgenres.name
   FROM Series
   JOIN Authors Subgenres
-  WHERE author_id = authors.id, subgenre_id = subgenres.id;"
+  WHERE author_id = authors.id subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
