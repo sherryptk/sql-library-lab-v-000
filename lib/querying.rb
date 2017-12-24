c975560cc830139db743d1e9679ef1caa86c0502
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 11:51:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_series_title_with_most_human_characters
   FROM Series
   JOIN CHARACTERS on series.id = series_id
   GROUP BY species
-  ORDER BY COUNT(species) DESC LIMIT 1;"
+  ORDER BY COUNT(species) LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
