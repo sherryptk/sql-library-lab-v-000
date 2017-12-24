1536ed80892c8e4b4fc589255ea24bb405d18b3f
Sherry El Garhy <sherryptk@gmail.com>
2017-12-24 11:42:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,9 +20,6 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  # "SELECT Authors.name, Subgenres.name
-  # FROM Authors
-  # JOIN Subgenres;"
   "SELECT Authors.name, Subgenres.name
   FROM Series
   JOIN Authors ON author_id = authors.id
